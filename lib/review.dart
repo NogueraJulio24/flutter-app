cc52ccb83e5cf95a4faa5be98a3253f99a4525b0
Julio Cesar Noguera <noguerajulio24@gmail.com>
2019-11-20 23:28:23 -0500

    review_list widget

--- lib/review.dart
+++ lib/review.dart
@@ -3,15 +3,87 @@ import 'package:flutter/material.dart';
 class Review extends StatelessWidget{
 
   String pat;
-  String name = "Julio";
-  String details = "1 review 5 photos";
-  String comment = "Este es un gran comentario";
+  String name;
+  String details;
+  String comment;
 
-  Review(this.pat, name);
+  Review(this.pat, this.name, this.details, this.comment);
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
+    //BLANK START
+    final start_border = Container(
+      margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0
+      ),
+      child: Icon(
+        Icons.star_border,
+        color: Color(0xFFf2C611),
+
+      ),
+    );
+
+    // HALF START
+    final start_half = Container(
+      margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0
+      ),
+      child: Icon(
+        Icons.star_half,
+        color: Color(0xFFf2C611),
+
+      ),
+    );
+
+    // COMPLETE START
+    final start = Container(
+      margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0
+      ),
+      child: Icon(
+        Icons.star,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+
+
+    // USER INFORMATION
+    final userInfo = Container(
+      margin: EdgeInsets.only(
+        left: 20.0
+      ),
+      child: Text(
+        details,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontSize: 13.0,
+          color: Color(0xFFa3a5a7)
+        ),
+      ),
+    );
+
+    // USER COMMENT
+    final userComment = Container(
+      margin: EdgeInsets.only(
+          left: 20.0
+      ),
+      child: Text(
+        comment,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+            fontSize: 13.0,
+          fontWeight: FontWeight.w900
+        ),
+      ),
+    );
+
+    // USER NAME
     final userName = Container(
       margin: EdgeInsets.only(
         left: 20.0
@@ -26,13 +98,17 @@ class Review extends StatelessWidget{
       ),
     );
 
-    final UserDetail = Column(
+    // DETAILS OF USER REVIEW
+    final userDetails = Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-
+        userName,
+        userInfo,
+        userComment
       ],
     );
 
+    // PHOTO OF THE USER
     final photo = Container(
       margin: EdgeInsets.only(
         top: 20.0,
@@ -52,7 +128,8 @@ class Review extends StatelessWidget{
 
     return Row(
       children: <Widget>[
-        photo
+        photo,
+        userDetails,
       ],
     );
   }
